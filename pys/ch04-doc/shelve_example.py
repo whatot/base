32114e20bcb282ea0892041945632eb5aa5377e8
whatot <whatot2@gmail.com>
2013-04-23 21:47:03 +0800

    [pys] add pop3_email imap_email

---
+++ pys/ch04-doc/shelve_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import shelve
+import apache_log_parser_regex
+
+logfile = open('access.log', 'r')
+shelve_file = shelve.open('access.s')
+
+for line in logfile:
+    d_line = apache_log_parser_regex.dictify_logline(line)
+    shelve_file[d_line['remote_host']] = \
+            shelve_file.setdefault(d_line['remote_host'], 0) + \
+            int(d_line['bytes_sent'])
+
+logfile.close()
+shelve_file.close()
