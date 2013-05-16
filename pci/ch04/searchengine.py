f92d9b5df05ecfba107db5766701e501c0af8ffb
whatot <whatot2@gmail.com>
2013-05-17 00:56:31 +0800

    [pci]add normalizescores and frequencyscore

--- pci/ch04/searchengine.py
+++ pci/ch04/searchengine.py
@@ -186,7 +186,7 @@ class searcher:
     def getscoredlist(self, rows, wordids):
         totalscores = dict([(row[0], 0) for row in rows])
 
-        weights = []
+        weights = [(1.0, self.frequencyscore(rows))]
 
         for (weight, scores) in weights:
             for url in totalscores:
@@ -205,6 +205,24 @@ class searcher:
                                for (url, score) in scores.items()], reverse=1)
         for (score, urlid) in rankedscores[0:10]:
             print('%f\t%s' % (score, self.geturlname(urlid)))
+
+    def frequencyscore(self, rows):
+        counts = dict([(row[0], 0) for row in rows])
+        for row in rows:
+            counts[row[0]] += 1
+        return self.normalizescores(counts)
+
+    def normalizescores(self, scores, smallIsBetter=0):
+        vsmall = 0.00001    # 避免被零整除
+        if smallIsBetter:
+            minscore = min(scores.values())
+            return dict([(u, float(minscore)/max(vsmall, l)) for (u, l) in
+                        scores.items()])
+        else:
+            maxscore = max(scores.values())
+            if maxscore == 0:
+                maxscore = vsmall
+            return dict([(u, float(c)/maxscore) for (u, c) in scores.items()])
 
 
 if __name__ == '__main__':
