af6cf11e16c676c9a4c8cee4b8253063bb46ea0d
whatot <whatot2@gmail.com>
2013-05-24 22:58:19 +0800

    [pci]add genetic to optimization.py

--- pci/ch05/README.md
+++ pci/ch05/README.md
@@ -2,5 +2,9 @@
 
 随机优化 stochastic optimization
 
-* randomoptimization
-* hillclimb
+* randomoptimization 随机
+* hillclimb 爬山法
+* annealingoptimize 模拟退火
+* geneticoptimize 遗传算法
+        * mutate 变异
+        * crossover 交叉
--- pci/ch05/optimization.py
+++ pci/ch05/optimization.py
@@ -161,6 +161,63 @@ class scheduleoptization:
         vec = [int(number) for number in vec]
         return vec
 
+    def geneticoptimize(self, domain, costf, popsize=50, step=1, mutprob=0.2,
+                        elite=0.2, maxiter=100):
+        # 可选参数：
+        #   popsize         种群大小
+        #   mutprob         种群的新成员是由变异而非交叉得来的概率
+        #   elite           种群中被认为是优解且被允许传入下一代的部分
+        #   maxiter         须运行多少代
+
+        # 变异操作
+        def mutate(vec):
+            i = random.randint(0, len(domain)-1)
+            if random.random() < 0.5 and vec[i] > domain[i][0]:
+                return vec[0:i] + [vec[i] - step] + vec[i+1:]
+            elif vec[i] < domain[i][1]:
+                return vec[0:i] + [vec[i] + step] + vec[i+1:]
+
+        # 交叉操作
+        def crossover(r1, r2):
+            i = random.randint(1, len(domain) - 2)
+            return r1[0:i] + r2[i:]
+
+        # 构造初始种群
+        pop = []
+        for i in range(popsize):
+            vec = [random.randint(domain[i][0], domain[i][1])
+                   for i in range(len(domain))]
+            pop.append(vec)
+
+        # 每一代中有多少优胜者
+        topelite = int(elite * popsize)
+
+        # 主循环
+        for i in range(maxiter):
+            scores = [(costf(v), v) for v in pop]
+            scores.sort()
+            ranked = [v for (s, v) in scores]
+
+            # 从纯粹的胜出者开始
+            pop = ranked[0:topelite]
+
+            # 添加变异和配对后的胜出者
+            while len(pop) < popsize:
+                if random.random() < mutprob:
+                    # 变异
+                    c = random.randint(0, topelite)
+                    pop.append(mutate(ranked[c]))
+                else:
+                    # 交叉
+                    c1 = random.randint(0, topelite)
+                    c2 = random.randint(0, topelite)
+                    pop.append(crossover(ranked[c1], ranked[c2]))
+
+            # 打印当前最优值
+            print(scores[0][0])
+
+        return scores[0][1]
+
 
 def testschedule(scheduleoptization):
     schopti = scheduleoptization()
@@ -193,5 +250,13 @@ def testannealingoptimize():
     schopti.printschedule(s)
 
 
+def testgeneticoptimize():
+    schopti = scheduleoptization()
+    domain = [(0, 9)] * (len(schopti.people) * 2)
+    s = schopti.geneticoptimize(domain, schopti.schedulecost)
+    print(schopti.schedulecost(s))
+    schopti.printschedule(s)
+
+
 if __name__ == '__main__':
-    testannealingoptimize()
+    testgeneticoptimize()
