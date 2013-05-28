d5a8974e26e19c722425f888c52fc940c0d7795c
whatot <whatot2@gmail.com>
2013-05-28 22:22:55 +0800

    [pci]basic start usage of unittest in ch06

--- pci/ch06/test_docclass.py
+++ pci/ch06/test_docclass.py
@@ -6,6 +6,12 @@ import docclass
 
 
 class test_docclass(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
     def test_getwords(self):
         words_source = 'disk*jdhs&342nhek[]989c12cnjshdsjkv774*j'
         words_result_dict = dict([('disk', 1), ('jdhs', 1),
@@ -13,6 +19,12 @@ class test_docclass(unittest.TestCase):
         self.assertDictEqual(words_result_dict,
                              docclass.getwords(words_source))
 
+    def test_incf(self):
+        clas = docclass.classifier(docclass.getwords)
+        origin_str = 'the quick brown for jumps over the lazy dog'
+        clas.train('the quick brown for jumps over the lazy dog', 'good')
+        str_tobe = docclass.getwords(origin_str)
+        self.assertDictEqual(clas.fc, str_tobe)
 
 if __name__ == '__main__':
     unittest.main()
