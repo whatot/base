3452f4b4589d5f5c4478a82146345191ea7b4e86
whatot <whatot2@gmail.com>
2013-06-03 21:47:12 +0800

    [pci]add the link of decision_tree_example.txt

--- pci/ch07/treepredict.py
+++ pci/ch07/treepredict.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 # -*- coding:utf-8 -*-
 
+# wget http://kiwitobes.com/tree/decision_tree_example.txt
 with open('decision_tree_example.txt', 'r') as tree:
     my_data = [line.split('\t') for line in tree]
 
