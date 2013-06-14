7dc89e50f1e226d048b98f30ccceddcd215fe7e3
whatot <whatot2@gmail.com>
2013-06-14 19:48:40 +0800

    add the text output for decision tree

--- pci/ch07/treepredict.py
+++ pci/ch07/treepredict.py
@@ -7,18 +7,6 @@ with open('decision_tree_example.txt', 'r') as tree:
 # print(my_data)
 
 
-# 对各种可能的结果进行计数(每一行数据的最后一列记录了这一计数结果)
-def uniquecounts(rows):
-    results = {}
-    for row in rows:
-        # 计数结果在最后一列
-        r = row[len(row) - 1]
-        if r not in results:
-            results[r] = 0
-        results[r] += 1
-    return results
-
-
 class decisionnode:
     def __init__(self, col=-1, value=None, results=None, tb=None, fb=None):
         # col 是待检验的判断条件所对应的列索引值
@@ -33,47 +21,113 @@ class decisionnode:
         self.tb = tb
         self.fb = fb
 
-    # 在某一列上对数据集合进行拆分，能够处理数值型数据或名词性数据。
-    def divideset(self, rows, column, value):
-        # 定义一个函数，令其告诉我们数据行属于第一组(true)还是第二组(false)
-        split_function = None
-        if isinstance(value, int) or isinstance(value, float):
-            split_function = lambda row: row[column] >= value
-        else:
-            split_function = lambda row: row[column] == value
 
-        # 将数据集拆分成两个几何，并返回
-        set1 = [row for row in rows if split_function(row)]
-        set2 = [row for row in rows if not split_function(row)]
-        return (set1, set2)
+# 在某一列上对数据集合进行拆分，能够处理数值型数据或名词性数据。
+def divideset(rows, column, value):
+    # 定义一个函数，令其告诉我们数据行属于第一组(true)还是第二组(false)
+    split_function = None
+    if isinstance(value, int) or isinstance(value, float):
+        split_function = lambda row: row[column] >= value
+    else:
+        split_function = lambda row: row[column] == value
 
-    # 随机放置的数据项出现于错误分类中的概率
-    def giniimpurify(self, rows):
-        total = len(rows)
-        counts = uniquecounts(rows)
-        imp = 0
-        for k1 in counts:
-            p1 = float(counts[k1]) / total
-            for k2 in counts:
-                if k1 == k2:
-                    continue
-                p2 = float(counts[k2]) / total
-                imp += p1 * p2
-        return imp
+    # 将数据集拆分成两个几何，并返回
+    set1 = [row for row in rows if split_function(row)]
+    set2 = [row for row in rows if not split_function(row)]
+    return (set1, set2)
 
-    # 熵是遍历所有可能的结果之后所得到的 p(x) * log(p(x)) 之和
-    # p(i) = frequency(outcome) = count(outcome) / count(total rows)
-    # Entropy = 针对所有结果的 p(i) * log(p(i)) 之和
-    def entropy(self, rows):
-        from math import log
-        log2 = lambda x: log(x) / log(2)
-        results = uniquecounts(rows)
-        # 此处开始计算熵的值
-        ent = 0.0
-        for r in results.keys():
-            p = float(results[r]) / len(rows)
-            ent = ent - p*log2(p)
-        return ent
+
+# 对各种可能的结果进行计数(每一行数据的最后一列记录了这一计数结果)
+def uniquecounts(rows):
+    results = {}
+    for row in rows:
+        # 计数结果在最后一列
+        r = row[len(row) - 1]
+        if r not in results:
+            results[r] = 0
+        results[r] += 1
+    return results
+
+
+# 随机放置的数据项出现于错误分类中的概率
+def giniimpurify(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+    for k1 in counts:
+        p1 = float(counts[k1]) / total
+        for k2 in counts:
+            if k1 == k2:
+                continue
+            p2 = float(counts[k2]) / total
+            imp += p1 * p2
+    return imp
+
+
+# 熵是遍历所有可能的结果之后所得到的 p(x) * log(p(x)) 之和
+# p(i) = frequency(outcome) = count(outcome) / count(total rows)
+# Entropy = 针对所有结果的 p(i) * log(p(i)) 之和
+def entropy(rows):
+    from math import log
+    log2 = lambda x: log(x) / log(2)
+    results = uniquecounts(rows)
+    # 此处开始计算熵的值
+    ent = 0.0
+    for r in results.keys():
+        p = float(results[r]) / len(rows)
+        ent = ent - p*log2(p)
+    return ent
+
+
+def buildtree(rows, scoref=entropy):
+    if len(rows) == 0:
+        return decisionnode()
+    current_score = scoref(rows)
+
+    # 定义一些变量以记录最佳拆分条件
+    best_gain = 0.0
+    best_criteria = None
+    best_sets = None
+
+    column_count = len(rows[0]) - 1
+    for col in range(0, column_count):
+        # 在当前列中生成一个由不同值构成的序列
+        column_values = {}
+        for row in rows:
+            column_values[row[col]] = 1
+        # 接下来根据这一列中的每个值，尝试对数据集进行拆分
+        for value in column_values.keys():
+            (set1, set2) = divideset(rows, col, value)
+
+            # 信息增益
+            p = float(len(set1)) / len(rows)
+            gain = current_score - p*scoref(set1) - (1-p)*scoref(set2)
+            if gain > best_gain and len(set1) > 0 and len(set2) > 0:
+                best_gain = gain
+                best_criteria = (col, value)
+                best_sets = (set1, set2)
+    # 创建子分支
+    if best_gain > 0:
+        trueBranch = buildtree(best_sets[0])
+        falseBranch = buildtree(best_sets[1])
+        return decisionnode(col=best_criteria[0], value=best_criteria[1],
+                            tb=trueBranch, fb=falseBranch)
+    else:
+        return decisionnode(results=uniquecounts(rows))
+
+
+def printtree(tree, indent=' '):
+    # 这是一个叶节点吗？
+    if tree.results is not None:
+        print(str(tree.results))
+    else:
+        # 打印判断条件
+        print(str(tree.col) + ':' + str(tree.value) + '? ')
+        # 打印分支
+        print(indent + 'T->'),
+        printtree(tree.tb, indent + '  ')
+        print(indent + 'F->'),
+        printtree(tree.fb, indent + '  ')
 
 
 def test_divideset():
@@ -101,5 +155,30 @@ def test_gini_entropy():
     print('tp.giniimpurify(set1)')
     print(tp.giniimpurify(set1))
 
+
+# $python --
+# 0:google?
+ # T-> 3:18?
+   # T-> 2:yes?
+     # T-> {'Basic\n': 1}
+     # F-> {'None\n': 1}
+   # F-> {'Premium\n': 3}
+ # F-> 3:19?
+   # T-> 0:kiwitobes?
+     # T-> 1:UK?
+       # T-> {'None\n': 1}
+       # F-> {'Basic': 1}
+     # F-> {'None\n': 1}
+   # F-> 0:slashdot?
+     # T-> {'None\n': 2}
+     # F-> 2:yes?
+       # T-> {'Basic\n': 3}
+       # F-> 1:UK?
+         # T-> {'Basic\n': 1}
+         # F-> {'None\n': 2}
+def testprinttree():
+    tree = buildtree(my_data)
+    printtree(tree)
+
 if __name__ == '__main__':
-    test_gini_entropy()
+    testprinttree()
