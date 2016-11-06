164c65eebf8bc1ebeaaf2619e0a18e5640d90715
whatot <whatot2@gmail.com>
2016-11-06 20:42:28 +0800

    add fib_test into base/lgo/example

---
+++ lgo/example/fib_test.go
@@ -0,0 +1,33 @@
+package example
+
+import "fmt"
+
+func fib(n int) chan int {
+	c := make(chan int)
+	go func() {
+		a, b := 0, 1
+		for i := 0; i < n; i++ {
+			a, b = b, a+b
+			c <- a
+		}
+		close(c)
+	}()
+	return c
+}
+
+func ExampleFib() {
+	for x := range fib(10) {
+		fmt.Println(x)
+	}
+	// Output:
+	// 1
+	// 1
+	// 2
+	// 3
+	// 5
+	// 8
+	// 13
+	// 21
+	// 34
+	// 55
+}
