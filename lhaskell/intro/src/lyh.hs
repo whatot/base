396553d7658de7e43c76bee404e5f6f2d6cfa624
whatot <whatot2@gmail.com>
2017-03-06 23:58:12 +0800

    format existing code and add TAGS into gitignore

--- lhaskell/intro/src/lyh.hs
+++ lhaskell/intro/src/lyh.hs
@@ -1,28 +1,30 @@
+module Lyh where
+
 firstLetter :: String -> String
 firstLetter "" = "Empty string, whoops!"
-firstLetter all@(x:xs) = "The first Letter of " ++ all ++ " is " ++ [x]
+firstLetter a@(x:_) = "The first Letter of " ++ a ++ " is " ++ [x]
 
-
-myCompare :: (Ord a) => a -> a -> Ordering
+myCompare
+  :: (Ord a)
+  => a -> a -> Ordering
 a `myCompare` b
-	| a == b	= EQ
-	| a <= b	= LT
-	| otherwise	= GT
-
+  | a == b = EQ
+  | a <= b = LT
+  | otherwise = GT
 
 bmiTell :: Double -> Double -> String
 bmiTell weight height
-	| bmi <= skinny	= "You're underweight, you emo, you!"
-	| bmi <= normal	= "You're supposedly normal.Pffft, I bet you're ugly!"
-	| bmi <= fat	= "You're fat! Lost some weight, fatty!"
-	| otherwise	= "You're a whale, congratulations!"
-	where	bmi = weight / height ^ 2
-		skinny = 18.5
-		normal = 25.0
-		fat = 30.0
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal.Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lost some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where
+    bmi = weight / height ^ 2
+    skinny = 18.5
+    normal = 25.0
+    fat = 30.0
+
 --(skinny, normal, fat) = (18.5, 25.0, 30.0)
-
-
 --global variable
 niceGreeting :: String
 niceGreeting = "Hello! So very nice to see you,"
@@ -35,29 +37,27 @@ greet "Juan" = niceGreeting ++ " Juan!"
 greet "Fernando" = niceGreeting ++ " Fernando!"
 greet name = badGreeting ++ " " ++ name
 
-
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
-	where	(f:_) = firstname
-		(l:_) = lastname
-
+  where
+    (f:_) = firstname
+    (l:_) = lastname
 
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi w h | (w, h) <- xs]
-	where bmi weight height = weight / height ^ 2
-
+  where
+    bmi weight height = weight / height ^ 2
 
 cylinder :: Double -> Double -> Double
 cylinder r h =
-	let	sideArea = 2 * pi * r * h
-		topArea = pi * r ^ 2
-	in sideArea + 2 * topArea
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in sideArea + 2 * topArea
 
 fac :: Integer -> Integer
 fac 0 = 1
-fac n | n > 0 = n * fac (n-1)
-
-
+fac n
+  | n > 0 = n * fac (n - 1)
 --ghci> 4 * (let a = 9 in a + 1) + 2
 --42
 --ghci> [let square x = x * x in (square 5, square 3, square 2)]
