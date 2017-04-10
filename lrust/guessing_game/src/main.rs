6bb5e00d631b1d2dc14cf9678854dc020254d704
whatot <whatot2@gmail.com>
2017-04-10 19:18:43 +0800

    add rust/guessing_game example

--- lrust/guessing_game/src/main.rs
+++ lrust/guessing_game/src/main.rs
@@ -1,17 +1,19 @@
 extern crate rand;
-
 use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
 
-fn guessing_game() {
+fn main() {
     println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 100);
 
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -27,8 +29,4 @@ fn guessing_game() {
             }
         }
     }
-}
-
-fn main() {
-    guessing_game()
 }
