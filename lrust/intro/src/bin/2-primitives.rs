1896dae360a7c9fd9ccfd7a8a22b09d2eb8c003e
whatot <whatot2@gmail.com>
2017-05-03 21:08:54 +0800

    add 2-primitives for tuple,array,slice or more rust

---
+++ lrust/intro/src/bin/2-primitives.rs
@@ -0,0 +1,117 @@
+use std::fmt::{self, Formatter, Display};
+use std::mem;
+
+fn print_literals() {
+
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
+}
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+struct Matrix(f32, f32, f32, f32);
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "( {} {} ) \n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
+fn print_tuples() {
+    // A tuple with a bunch of different types
+    let long_tuple = (1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true);
+    println!("first of long tuple: {}", long_tuple.0);
+    println!("second of long tuple: {}", long_tuple.1);
+
+    // Tuples can be tuple members
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    //tuples can be destructured to create bindings
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
+}
+
+// This function borrows a slice
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
+}
+
+fn print_arrays_slices() {
+    // Fixed-size array (type signature is superfluous)
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Indexing starts at 0
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+
+    // `len` returns the size of the array
+    println!("array size: {}", xs.len());
+
+    // Arrays are stack allocated
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    // Out of bound indexing yields a panic
+    // println!("{}", xs[5]);
+
+    // Arrays can be automatically borrowed as slices
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    // All elements can be initialized to the same value
+    let ys: [i32; 500] = [0; 500];
+
+    // Slices can point to a section of an array
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1..4]);
+}
+
+fn main() {
+    print_literals();
+    println!();
+    print_tuples();
+    println!();
+    print_arrays_slices();
+}
