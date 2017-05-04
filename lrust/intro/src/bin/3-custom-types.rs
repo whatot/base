af2942090c9c59e54855f6181d957b5af4174638
whatot <whatot2@gmail.com>
2017-05-04 14:57:15 +0800

    add more enum and usage in 3-custom-types

--- lrust/intro/src/bin/3-custom-types.rs
+++ lrust/intro/src/bin/3-custom-types.rs
@@ -2,11 +2,15 @@ fn main() {
     print_structures();
     println!();
     print_enums();
+    println!();
+    print_c_enums();
+    println!();
+    print_linked_list();
 }
 
 
 // A unit struct
-struct Nil;
+struct OneUnit;
 
 // A tuple struct
 struct Pair(i32, f32);
@@ -57,7 +61,7 @@ fn print_structures() {
     };
 
     // Instantiate a unit struct
-    let _nil = Nil;
+    let _nil = OneUnit;
 
     // Instantiate a tuple struct
     let pair = Pair(1, 0.1);
@@ -112,3 +116,93 @@ fn print_enums() {
     inspect(rebecca);
     inspect(rohan);
 }
+
+#[allow(dead_code)]
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+#[allow(dead_code)]
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+fn print_c_enums() {
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+}
+
+use List::*;
+enum List {
+    // Cons: Tuple struct that wraps an element and a pointer to the next node
+    Cons(u32, Box<List>),
+    // Nil: A node that signifies the end of the linked list
+    Nil,
+}
+
+impl List {
+    // Create an empty list
+    fn new() -> List {
+        // `Nil` has type `List`
+        Nil
+    }
+
+    // Consume a list, and return the same list with a new element at its front
+    fn prepend(self, elem: u32) -> List {
+        // `Cons` also has type List
+        Cons(elem, Box::new(self))
+    }
+
+    // Return the length of the list
+    fn len(&self) -> u32 {
+        // `self` has to be matched, because the behavior of this method
+        // depends on the variant of `self`
+        // `self` has type `&List`, and `*self` has type `List`, matching on a
+        // concrete type `T` is preferred over a match on a reference `&T`
+        match *self {
+            // Can't take ownership of the tail, because `self` is borrowed;
+            // instead take a reference to the tail
+            Cons(_, ref tail) => 1 + tail.len(),
+            // End Case: An empty list has zero length
+            Nil => 0,
+        }
+    }
+
+    // Return representation of the list as a (heap allocated) string
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                // `format!` is similar to `print!`, but returns a heap
+                // allocated string instead of printing to the console
+                format!("{}, {}", head, tail.stringify())
+            }
+            Nil => format!("Nil"),
+        }
+    }
+}
+
+fn print_linked_list() {
+    // Create an empty linked list
+    let mut list = List::new();
+
+    // Append some elements
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    // Show the final state of the list
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+}
+
+// constants
+#[allow(dead_code)]
+static LANGUAGE: &'static str = "rust";
+#[allow(dead_code)]
+const THRESHOLD: i32 = 10;
