9851c82c2d02eae1d73dc7763386498558fbbffd
whatot <whatot2@gmail.com>
2017-05-11 23:08:13 +0800

    add super and self about into lrust/intro

--- lrust/intro/src/bin/9-modules.rs
+++ lrust/intro/src/bin/9-modules.rs
@@ -4,6 +4,8 @@ fn main() {
     c2_struct_visibility();
     println!();
     c3_use();
+    println!();
+    c4_super_self();
 }
 
 mod c1_my {
@@ -126,4 +128,52 @@ fn c3_use() {
         println!("Leaving block");
     }
     function();
+}
+
+mod cool {
+    pub fn function() {
+        println!("called cool::function()");
+    }
+}
+
+mod c4_my {
+    fn function() {
+        println!("called c4_my::function()");
+    }
+
+    mod cool {
+        pub fn function() {
+            println!("called c4_my::cool::function()");
+        }
+    }
+
+    pub fn indirect_call() {
+        // Let's access all the functions named `function` from this scope!
+        println!("called c4_my::indirect_call()");
+
+        // The `self` keyword refers to the current module scope -
+        // in this case `c4_my`.
+        // Calling `self::function()` and calling `function()` directly both
+        // give the same result, because they refer to the same function.
+        self::function();
+        function();
+
+        // We can also use `self` to access another module inside `my`:
+        self::cool::function();
+
+        // The `super` keyword refers to the parent scope
+        // (outside the `my` module).
+        super::function();
+
+        // This will bind to the `cool::function` in the *crate* scope.
+        // In this case the crate scope is the outermost scope.
+        {
+            use cool::function as root_function;
+            root_function();
+        }
+    }
+}
+
+fn c4_super_self() {
+    c4_my::indirect_call();
 }
