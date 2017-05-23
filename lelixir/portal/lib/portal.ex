bc4410604cdfd58aacadc26f3864ae90c34e9abd
whatot <whatot2@gmail.com>
2017-05-23 14:36:16 +0800

    add portal and portal.door in lelixir/portal

--- lelixir/portal/lib/portal.ex
+++ lelixir/portal/lib/portal.ex
@@ -3,16 +3,33 @@ defmodule Portal do
   Documentation for Portal.
   """
 
+  defstruct [:left, :right]
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Portal.hello
-      :world
-
+  Start transfering `data` from `left` to `right`.
   """
-  def hello do
-    :world
+  def transfer(left, right, data) do
+    # First add all data to the portal on the left
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    # Returns a portal struct we will use next
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Push data to the right in the given `portal`.
+  """
+  def push_right(portal) do
+    # See if we can pop data from left. If so, push the
+    # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left) do
+      :error   -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+
+    # Let's return the portal itself
+    portal
   end
 end
