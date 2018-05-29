69e515b02799404f69b2f4ea4f9c8cce20d7cdb7
whatot <whatot2@gmail.com>
2018-05-29 16:06:24 +0800

    format guessing and update Cargo.lock

--- lrust/intro/src/bin/guessing.rs
+++ lrust/intro/src/bin/guessing.rs
@@ -10,9 +10,9 @@ fn guessing_game() {
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect(
+            "Failed to read line",
+        );
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
