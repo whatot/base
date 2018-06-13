78629fd64e4deb0bb34e1c0d6af9d2cc900c4907
whatot <whatot2@gmail.com>
2018-06-13 22:57:37 +0800

    add multi_sender channel in intro

--- lrust/intro/src/bin/thread-check.rs
+++ lrust/intro/src/bin/thread-check.rs
@@ -1,10 +1,10 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
 use std::sync::mpsc;
 use std::thread;
 
-fn main() {}
-
 #[test]
-fn test_single_async_channel() {
+fn test_channel_single_sender() {
     let num = 566;
     let (tx, rx): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = mpsc::channel();
     thread::spawn(move || {
@@ -12,4 +12,21 @@ fn test_single_async_channel() {
     });
 
     assert_eq!(num, rx.recv().unwrap());
+}
+
+#[test]
+fn test_channel_multi_sender() {
+    let thread_num = 32;
+    let (tx, rx): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = mpsc::channel();
+    for id in 0..thread_num {
+        let thread_tx = tx.clone();
+        thread::spawn(move || {
+            thread_tx.send(id).unwrap();
+        });
+    }
+
+    let result_set: HashSet<i32> = HashSet::from_iter(0..thread_num);
+    for _ in &result_set {
+        assert!(result_set.contains(&rx.recv().unwrap()));
+    }
 }
