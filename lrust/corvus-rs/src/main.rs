4026adc19f558dc72592f5648e0f7c4e8a5e83d9
whatot <whatot2@gmail.com>
2018-09-14 22:55:03 +0800

    add basic entry for corvus-rs

--- lrust/corvus-rs/src/main.rs
+++ lrust/corvus-rs/src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("Corvus-rs")
+        .version("0.1")
+        .author("whatot whatot2@gmail.com")
+        .about("A lightweight redis cluster proxy")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        ).get_matches();
 }
