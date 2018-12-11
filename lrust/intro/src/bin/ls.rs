12b53c562a75c2792efae8e7aaa4c7a6bc0a34c1
whatot <whatot2@gmail.com>
2018-12-11 11:49:06 +0800

    replace "r#try!()" with "?"

--- lrust/intro/src/bin/ls.rs
+++ lrust/intro/src/bin/ls.rs
@@ -1,13 +1,13 @@
 use std::env;
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 fn ls_current() -> Result<(), Box<Error>> {
-    let here = r#try!(env::current_dir());
+    let here = env::current_dir()?;
     println!("list current dir: {}", here.display());
-    for entry in r#try!(fs::read_dir(&here)) {
-        let path = r#try!(entry).path();
-        let md = r#try!(fs::metadata(&path));
+    for entry in fs::read_dir(&here)? {
+        let path = entry?.path();
+        let md = fs::metadata(&path)?;
         println!("    {} {} bytes", path.display(), md.len());
     }
     Ok(())
