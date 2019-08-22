a5efac414347c7329eb4a78ae323b86c049730b4
whatot <whatot2@gmail.com>
2019-08-22 22:01:28 +0800

    use latest nginx to setup webdav

--- docker/webdav/apache-webdav/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
-chown root:www-data /etc/apache2/webdav.password
-chmod 640 /etc/apache2/webdav.password
-
-apache2 -D FOREGROUND
