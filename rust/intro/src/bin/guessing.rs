b937a7a4359f314758c3123184d8ba7019858602
whatot <whatot2@gmail.com>
2022-10-06 17:08:25 +0800

    feat(intro): update to latest rand

--- rust/intro/src/bin/guessing.rs
+++ rust/intro/src/bin/guessing.rs
@@ -4,7 +4,7 @@ use std::io;
 
 fn guessing_game() {
     println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 100);
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
     loop {
         println!("Please input your guess.");
