68ce02b0dbb7c0498568726600867b24bcd43905
whatot <whatot2@gmail.com>
2023-12-28 00:11:56 +0800

    feat: add box animation

--- rust/crates/sokoban-rs/src/entities.rs
+++ rust/crates/sokoban-rs/src/entities.rs
@@ -26,10 +26,10 @@ pub fn create_box(world: &mut World, position: Position, colour: BoxColour) {
     world
         .create_entity()
         .with(Position { z: 10, ..position })
-        .with(Renderable::new_static(format!(
-            "/images/box_{}.png",
-            colour
-        )))
+        .with(Renderable::new_animated(vec![
+            format!("/images/box_{}_1.png", colour),
+            format!("/images/box_{}_2.png", colour),
+        ]))
         .with(Box { colour })
         .with(Movable {})
         .build();
