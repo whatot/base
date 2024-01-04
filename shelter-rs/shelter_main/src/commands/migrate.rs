8882e666d58c5a2043e528565fa6fc357c65805c
whatot <whatot2@gmail.com>
2024-01-05 02:19:13 +0800

    feat: support sea-orm with migration for shelter

---
+++ shelter-rs/shelter_main/src/commands/migrate.rs
@@ -0,0 +1,28 @@
+use anyhow::Ok;
+use clap::{ArgMatches, Command};
+use migration::{Migrator, MigratorTrait};
+use sea_orm::Database;
+
+use crate::settings::Settings;
+
+pub fn configure() -> Command {
+    Command::new("migrate").about("Run database migrations")
+}
+
+pub fn handle(matches: &ArgMatches, settings: &Settings) -> anyhow::Result<()> {
+    if let Some(_match) = matches.subcommand_matches("migrate") {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async move {
+                let db_url = settings.database.url.clone().unwrap_or("".to_string());
+                let conn = Database::connect(db_url)
+                    .await
+                    .expect("Database connection failed");
+                Migrator::up(&conn, None).await.unwrap();
+            })
+    }
+
+    Ok(())
+}
--- shelter-rs/shelter_main/src/commands/mod.rs
+++ shelter-rs/shelter_main/src/commands/mod.rs
@@ -3,17 +3,20 @@ use clap::{ArgMatches, Command};
 
 use crate::settings::Settings;
 mod hello;
+mod migrate;
 mod serve;
 
 pub fn configure(command: Command) -> Command {
     command
         .subcommand(hello::configure())
         .subcommand(serve::configure())
+        .subcommand(migrate::configure())
 }
 
 pub fn handle(matches: &ArgMatches, settings: &Settings) -> anyhow::Result<()> {
     hello::handle(matches, settings)?;
     serve::handle(matches, settings)?;
+    migrate::handle(matches, settings)?;
 
     Ok(())
 }
