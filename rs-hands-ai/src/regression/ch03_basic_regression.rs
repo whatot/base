cd6d544174d87a98ed4db45274f8758e72ac49ff
whatot <whatot2@gmail.com>
2024-01-18 13:24:21 +0800

    feat: impl test_solve_by_matrix

--- rs-hands-ai/src/regression/ch03_basic_regression.rs
+++ rs-hands-ai/src/regression/ch03_basic_regression.rs
@@ -1,4 +1,5 @@
-use ndarray::{Array1, Axis};
+use ndarray::{Array1, Array2, Axis};
+use ndarray_linalg::Inverse;
 
 // ndarray
 // https://docs.rs/ndarray/latest/ndarray/doc/ndarray_for_numpy_users/index.html
@@ -23,22 +24,45 @@ pub fn square_loss(x: &Array1<u32>, y: &Array1<u32>, w0: f64, w1: f64) -> f64 {
         .sum()
 }
 
+/// 最小二乘法矩阵求解
+///  (x.T * x).I * x.T * y
+#[allow(dead_code)]
+pub fn least_squares_matrix(x: &Array2<f64>, y: &Array2<f64>) -> anyhow::Result<(f64, f64)> {
+    // 计算x的转置乘以x
+    let xtx = x.t().dot(x);
+
+    // 计算x的逆
+    let inv_xtx = xtx.inv()?;
+
+    // 计算x的转置乘以y
+    let xt_y = x.t().dot(y);
+
+    // 最后，计算w = inv_xtx * xt_y
+    let w = inv_xtx.dot(&xt_y);
+
+    Ok((w[[0, 0]], w[[1, 0]]))
+}
+
 #[cfg(test)]
 mod tests {
 
     use crate::assert_approx_eq;
+    use crate::regression::ch03_basic_regression::least_squares_matrix;
 
     use super::least_squares_algebraic;
     use super::square_loss;
     use anyhow::Ok;
     use linfa::traits::Fit;
-    use ndarray::array;
     use ndarray::concatenate;
     use ndarray::Array;
+    use ndarray::Array1;
     use ndarray::Axis;
 
+    const X_ARRAY: [u32; 10] = [56, 72, 69, 88, 102, 86, 76, 79, 94, 74];
+    const Y_ARRAY: [u32; 10] = [92, 102, 86, 110, 130, 99, 96, 102, 105, 92];
+
     #[test]
-    fn test_linfa_linear_regression() -> anyhow::Result<()> {
+    fn test_solve_by_linfa() -> anyhow::Result<()> {
         let dataset = linfa_datasets::diabetes();
 
         let lin_reg = linfa_linear::LinearRegression::new();
@@ -51,9 +75,9 @@ mod tests {
     }
 
     #[test]
-    fn test_solve_regression_by_algebra() -> anyhow::Result<()> {
-        let x = array![56, 72, 69, 88, 102, 86, 76, 79, 94, 74];
-        let y = array![92, 102, 86, 110, 130, 99, 96, 102, 105, 92];
+    fn test_solve_by_algebra() -> anyhow::Result<()> {
+        let x = Array1::from_iter(X_ARRAY);
+        let y = Array1::from_iter(Y_ARRAY);
 
         let (w0, w1) = least_squares_algebraic(&x, &y);
         let loss = square_loss(&x, &y, w0, w1);
@@ -67,19 +91,27 @@ mod tests {
     }
 
     #[test]
-    fn test_solve_regression_by_matrix() -> anyhow::Result<()> {
-        let x = vec![56, 72, 69, 88, 102, 86, 76, 79, 94, 74];
-        let y = vec![92, 102, 86, 110, 130, 99, 96, 102, 105, 92];
+    fn test_solve_by_matrix() -> anyhow::Result<()> {
+        let x: Vec<f64> = X_ARRAY.iter().map(|&e| e as f64).collect();
+        let y: Vec<f64> = Y_ARRAY.iter().map(|&e| e as f64).collect();
 
         let x_matrix = concatenate![
             Axis(1),
             Array::ones((x.len(), 1)),
             Array::from_shape_vec((x.len(), 1), x)?
         ];
-        println!("{}", x_matrix);
+        assert_eq!(2, x_matrix.ndim());
+        assert_eq!(10, x_matrix.len_of(Axis(0)));
+        assert_eq!(2, x_matrix.len_of(Axis(1)));
 
         let y_matrix = Array::from_shape_vec((y.len(), 1), y)?;
-        println!("{}", y_matrix);
+        assert_eq!(2, y_matrix.ndim());
+        assert_eq!(10, y_matrix.len_of(Axis(0)));
+        assert_eq!(1, y_matrix.len_of(Axis(1)));
+
+        let (w0, w1) = least_squares_matrix(&x_matrix, &y_matrix)?;
+        assert_approx_eq!(w0, 41.33509168550616f64);
+        assert_approx_eq!(w1, 0.7545842753077117f64);
 
         Ok(())
     }
