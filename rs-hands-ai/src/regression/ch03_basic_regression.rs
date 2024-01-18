76fbf1a1a9e367f13f99cc03949289b2f6771ce6
whatot <whatot2@gmail.com>
2024-01-18 14:26:08 +0800

    feat: impl test_solve_by_linfa and all use f64

--- rs-hands-ai/src/regression/ch03_basic_regression.rs
+++ rs-hands-ai/src/regression/ch03_basic_regression.rs
@@ -7,8 +7,8 @@ use ndarray_linalg::Inverse;
 
 /// 最小二乘法代数求解
 #[allow(dead_code)]
-pub fn least_squares_algebraic(x: &Array1<u32>, y: &Array1<u32>) -> (f64, f64) {
-    let n = x.len_of(Axis(0)) as u32;
+pub fn least_squares_algebraic(x: &Array1<f64>, y: &Array1<f64>) -> (f64, f64) {
+    let n = x.len_of(Axis(0)) as f64;
     let w1 = (n * (x * y).sum() - x.sum() * y.sum()) as f64
         / (n * (x * x).sum() - x.sum() * x.sum()) as f64;
     let w0 = ((x * x).sum() * y.sum() - x.sum() * (x * y).sum()) as f64
@@ -18,7 +18,7 @@ pub fn least_squares_algebraic(x: &Array1<u32>, y: &Array1<u32>) -> (f64, f64) {
 
 /// 平方损失函数
 #[allow(dead_code)]
-pub fn square_loss(x: &Array1<u32>, y: &Array1<u32>, w0: f64, w1: f64) -> f64 {
+pub fn square_loss(x: &Array1<f64>, y: &Array1<f64>, w0: f64, w1: f64) -> f64 {
     (y.mapv(|yv| yv as f64) - x.mapv(|value| w0 + w1 * value as f64))
         .mapv(|value| value * value)
         .sum()
@@ -53,26 +53,29 @@ mod tests {
     use super::square_loss;
     use anyhow::Ok;
     use linfa::traits::Fit;
+    use linfa::Dataset;
     use ndarray::concatenate;
     use ndarray::Array;
     use ndarray::Array1;
     use ndarray::Axis;
 
-    const X_ARRAY: [u32; 10] = [56, 72, 69, 88, 102, 86, 76, 79, 94, 74];
-    const Y_ARRAY: [u32; 10] = [92, 102, 86, 110, 130, 99, 96, 102, 105, 92];
+    const X_ARRAY: [f64; 10] = [56., 72., 69., 88., 102., 86., 76., 79., 94., 74.];
+    const Y_ARRAY: [f64; 10] = [92., 102., 86., 110., 130., 99., 96., 102., 105., 92.];
     const EXPECT_W0: f64 = 41.33509168550616f64;
     const EXPECT_W1: f64 = 0.7545842753077117f64;
     const EXPECT_LOSS: f64 = 447.69153479025357f64;
 
     #[test]
     fn test_solve_by_linfa() -> anyhow::Result<()> {
-        let dataset = linfa_datasets::diabetes();
+        let x = Array::from_shape_vec((X_ARRAY.len(), 1), X_ARRAY.to_vec())?;
+        let y = Array1::from_iter(Y_ARRAY);
+        let dataset = Dataset::new(x, y);
 
         let lin_reg = linfa_linear::LinearRegression::new();
         let model = lin_reg.fit(&dataset)?;
 
-        println!("intercept:  {}", model.intercept());
-        println!("parameters: {}", model.params());
+        assert_approx_eq!(model.intercept(), EXPECT_W0);
+        assert_approx_eq!(model.params()[0], EXPECT_W1);
 
         Ok(())
     }
@@ -95,19 +98,16 @@ mod tests {
 
     #[test]
     fn test_solve_by_matrix() -> anyhow::Result<()> {
-        let x: Vec<f64> = X_ARRAY.iter().map(|&e| e as f64).collect();
-        let y: Vec<f64> = Y_ARRAY.iter().map(|&e| e as f64).collect();
-
         let x_matrix = concatenate![
             Axis(1),
-            Array::ones((x.len(), 1)),
-            Array::from_shape_vec((x.len(), 1), x)?
+            Array::ones((X_ARRAY.len(), 1)),
+            Array::from_shape_vec((X_ARRAY.len(), 1), X_ARRAY.to_vec())?
         ];
         assert_eq!(2, x_matrix.ndim());
         assert_eq!(10, x_matrix.len_of(Axis(0)));
         assert_eq!(2, x_matrix.len_of(Axis(1)));
 
-        let y_matrix = Array::from_shape_vec((y.len(), 1), y)?;
+        let y_matrix = Array::from_shape_vec((Y_ARRAY.len(), 1), Y_ARRAY.to_vec())?;
         assert_eq!(2, y_matrix.ndim());
         assert_eq!(10, y_matrix.len_of(Axis(0)));
         assert_eq!(1, y_matrix.len_of(Axis(1)));
