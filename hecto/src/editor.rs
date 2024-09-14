9746ca512e30518d05858a009093992828331fdd
whatot <whatot2@gmail.com>
2024-09-14 16:26:26 +0800

    feat: split logic to editor.rs

---
+++ hecto/src/editor.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Read};
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor {}
+    }
+
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+
+        for b in io::stdin().bytes() {
+            match b {
+                Ok(b) => {
+                    let c = b as char;
+                    if c.is_control() {
+                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                    } else {
+                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                    }
+
+                    if c == 'q' {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    println!("Error: {e}");
+                    break;
+                }
+            }
+        }
+
+        disable_raw_mode().unwrap();
+    }
+}
--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,30 +1,7 @@
-use std::io::{self, Read};
-
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                } else {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                }
-
-                if c == 'q' {
-                    break;
-                }
-            }
-            Err(e) => {
-                println!("Error: {e}");
-                break;
-            }
-        }
-    }
-
-    disable_raw_mode().unwrap();
+    let editor = Editor::default();
+    editor.run();
 }
