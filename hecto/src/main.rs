ebc50b77d68507d2c197fc2c53d67e18926f3ebc
whatot <whatot2@gmail.com>
2024-09-14 15:38:30 +0800

    feat: add crossterm dep for raw mode

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,8 +1,21 @@
 use std::io::{self, Read};
 
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
 fn main() {
+    enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        }
+
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
     }
 }
