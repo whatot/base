f7576ec376cf3485669a5bbf0614ca6138e22a8a
whatot <whatot2@gmail.com>
2024-09-14 16:15:51 +0800

    feat: avoid unwrap for binary

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -4,18 +4,27 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 fn main() {
     enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
 
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(e) => {
+                println!("Error: {e}");
+                break;
+            }
         }
     }
+
+    disable_raw_mode().unwrap();
 }
