986a464a7572c7baba0443b7ff19af707119734f
whatot <whatot2@gmail.com>
2024-09-25 17:19:22 +0800

    feat: add resource/test-numeric-literal-highlighting.rs

---
+++ hecto/resource/test-numeric-literal-highlighting.rs
@@ -0,0 +1,18 @@
+// Integers:
+// 1 2 3 4 5 6 7 8 9 0, 100 200 300 400 500 1>2 1+1=2 
+// Floats:
+// 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 0.0
+// Scientific Notation:
+// 1e10, 20e50, 10.3e5, 
+// Visual Separators:
+// 1_00, 1_000_1, 1_000_000_000
+
+
+// Invalid Integers:
+// 1a 2b 3c 4d 5e 6f 7g 8h 9i 0j, 100a200b300c400d500, u32, i8, f64, 1,2,3,4,5
+// Invalid Floats:
+// 1.1.2, 2.2.3, 3.3.4, 4.4.5, 5.5.6, 6.6.7, 7.7.8, 8.8.9, 9.9.0, 0.0.1
+// Invalid Scientific Notation:
+// 1e, e3, e, 1e2e, 5.8e10.1
+// Invalid Visual Separators:
+// _100_1, 100_, 1_00_, _
