d310bb025ed38ae9571e56bb70002616a2d356cc
whatot <whatot2@gmail.com>
2026-06-20 22:01:38 +0800

    chore: resolve rust dependency alerts

--- rust/crates/bunny-rs/src/main.rs
+++ rust/crates/bunny-rs/src/main.rs
@@ -1,25 +1,33 @@
-use rocket::response::Redirect;
+use std::collections::HashMap;
 
-#[macro_use]
-extern crate rocket;
+use axum::{Router, extract::Query, http::StatusCode, response::Redirect, routing::get};
 
 mod github;
 mod google;
 mod twitter;
 mod utils;
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, search])
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/search", get(search));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
 
-#[get("/")]
-fn index() -> &'static str {
+async fn index() -> &'static str {
     "Hello World"
 }
 
-#[get("/search?<cmd>")]
-fn search(cmd: String) -> Redirect {
+async fn search(Query(params): Query<HashMap<String, String>>) -> Result<Redirect, StatusCode> {
+    let Some(cmd) = params.get("cmd") else {
+        return Err(StatusCode::BAD_REQUEST);
+    };
+
     println!("You typed in: {}", cmd);
     let command = utils::get_command_from_query_string(&cmd);
 
@@ -28,5 +36,5 @@ fn search(cmd: String) -> Redirect {
         "gh" => github::construct_github_url(&cmd),
         _ => google::construct_google_search_url(&cmd),
     };
-    Redirect::to(redirect_url)
+    Ok(Redirect::to(&redirect_url))
 }
